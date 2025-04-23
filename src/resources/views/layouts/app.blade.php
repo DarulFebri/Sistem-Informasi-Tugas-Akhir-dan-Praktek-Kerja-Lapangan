9ba1496c529acee6ea48fd5b0e1e850e12ef5c49
Darul Febri <darulfer097@gmail.com>
2025-04-23 06:55:23 -0400

    Fix Fitur Dashboard

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <ul class="nav flex-column">
             <li>
                 <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#kelolaBimbingan" aria-expanded="false" aria-controls="kelolaBimbingan">
-                    <i class="bi bi-people"></i> Kelola Bimbingan <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
+                    <i class="bi bi-people"></i> <span class="link-text">Kelola Bimbingan</span> <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
                 </a>
                 <div class="collapse submenu" id="kelolaBimbingan">
                     <a class="nav-link" href="#">Dosen Pembimbing</a>
@@ -26,7 +26,7 @@
             </li>
             <li>
                 <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#pengajuanJudul" aria-expanded="false" aria-controls="pengajuanJudul">
-                    <i class="bi bi-journal-text"></i> Pengajuan Judul <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
+                    <i class="bi bi-journal-text"></i> <span class="link-text">Pengajuan Judul</span> <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
                 </a>
                 <div class="collapse submenu" id="pengajuanJudul">
                     <a class="nav-link" href="#">Judul Masuk</a>
@@ -35,7 +35,7 @@
             </li>
             <li>
                 <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#pengajuanTA" aria-expanded="false" aria-controls="pengajuanTA">
-                    <i class="bi bi-journals"></i> Pengajuan TA <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
+                    <i class="bi bi-journals"></i> <span class="link-text">Pengajuan TA</span> <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
                 </a>
                 <div class="collapse submenu" id="pengajuanTA">
                     <a class="nav-link" href="#">TA Pengerjaan</a>
@@ -45,7 +45,7 @@
             </li>
             <li>
                 <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#dosen" aria-expanded="false" aria-controls="dosen">
-                    <i class="bi bi-person"></i> Dosen <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
+                    <i class="bi bi-person"></i> <span class="link-text">Dosen</span> <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
                 </a>
                 <div class="collapse submenu" id="dosen">
                     <a class="nav-link" href="#">Data Dosen</a>
@@ -53,14 +53,14 @@
             </li>
             <li>
                 <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#mahasiswa" aria-expanded="false" aria-controls="mahasiswa">
-                    <i class="bi bi-mortarboard"></i> Mahasiswa <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
+                    <i class="bi bi-mortarboard"></i> <span class="link-text">Mahasiswa</span> <i class="bi bi-caret-down-fill ms-auto dropdown-icon"></i>
                 </a>
                 <div class="collapse submenu" id="mahasiswa">
                     <a class="nav-link" href="#">Data Mahasiswa</a>
                 </div>
             </li>
-            <li><a class="nav-link" href="#"><i class="bi bi-book"></i> Program Studi</a></li>
-            <li><a class="nav-link" href="#"><i class="bi bi-calendar-event"></i> Jadwal</a></li>
+            <li><a class="nav-link" href="#"><i class="bi bi-book"></i> <span class="link-text">Program Studi</span></a></li>
+            <li><a class="nav-link" href="#"><i class="bi bi-calendar-event"></i> <span class="link-text">Jadwal</span></a></li>
         </ul>
     </div>
 
@@ -81,15 +81,21 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
+        const sidebarToggle = document.getElementById('sidebarToggle');
+        const body = document.body;
         const navLinksWithSubmenu = document.querySelectorAll('.sidebar .nav-link[data-bs-toggle="collapse"]');
         const submenuLinks = document.querySelectorAll('.sidebar .submenu .nav-link');
+
+        sidebarToggle.addEventListener('click', function() {
+            body.classList.toggle('sidebar-collapsed');
+        });
 
         navLinksWithSubmenu.forEach(link => {
             link.addEventListener('click', function() {
                 const targetId = this.getAttribute('data-bs-target');
                 const currentlyOpen = document.querySelector('.submenu.show');
 
-                if (currentlyOpen && '#' + currentlyOpen.id !== targetId) {
+                if (currentlyOpen && '#' + currentlyOpen.id !== targetId && !body.classList.contains('sidebar-collapsed')) {
                     const bsCollapse = new bootstrap.Collapse(currentlyOpen);
                     bsCollapse.hide();
                     const currentlyOpenLink = document.querySelector(`.nav-link[data-bs-target="#${currentlyOpen.id}"]`);
